805b94e5e3c60e3d6a53ed9f5fede98d116a153e
ezhomelabs <haiwei.he@gmail.com>
2024-07-30 15:21:41 +1000

    update

--- generate_index.py
+++ generate_index.py
@@ -1,5 +1,17 @@
 import os
 
+def generate_root_index():
+    directory = 'packages'
+
+    index_path = os.path.join(directory, '..', 'index.html')
+    with open(index_path, 'w') as f:
+        f.write('<html><body>\n')
+        for filename in os.listdir(directory):
+            if filename != 'index.html':
+                f.write(f'<a href="packages/{filename}/dist">{filename}</a><br>\n')
+        f.write('</body></html>\n')
+    print(f"Index generated at {index_path}")
+
 def generate_index(directory):
     index_path = os.path.join(directory, 'index.html')
     with open(index_path, 'w') as f:
@@ -15,6 +27,8 @@ def process_directory(root_dir):
         # Check if we are in a 'dist' directory
         if os.path.basename(dirpath) == 'dist':
             generate_index(dirpath)
+
+generate_root_index()
 
 # Root directory where packages are stored
 root_dir = 'packages'
